5e591fe62d55d410ff8554df3650e72ed6eaf581
shashank027 <sharma.shashank027@gmail.com>
2016-08-15 15:01:03 +0530

    Update LMP91000.h

--- firmware/LMP91000.h
+++ firmware/LMP91000.h
@@ -1,11 +1,11 @@
 #ifndef LMP91000_H
 #define LMP91000_H
 
-#if defined(ARDUINO) && ARDUINO >= 100
-#include <Arduino.h>
-#else
-#include <WProgram.h>
-#endif
+// #if defined(ARDUINO) && ARDUINO >= 100
+// #include <Arduino.h>
+// #else
+// #include <WProgram.h>
+// #endif
 
 #include <Wire.h>
 
